547ecbb72599968a95008844e67bec5b0e5ebd71
tuco5 <tudor.comparan@gmail.com>
2025-08-07 18:20:16 -0600

    footer

---
+++ src/components/template/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="mt-auto w-full bg-blue-800 p-4 text-white">
+      <p className="text-center">© 2025 El Maestro de la Brocha</p>
+    </footer>
+  );
+}
