437318fc5d8894e5f7fd10820afa34a051a3633e
Tudor Comparan <tudor.comparan@gmail.com>
2025-08-13 15:37:15 -0600

    Fix: Page exports (#3)
    * export components properly
    
    * optimize image
    
    * refine styles
    
    * more responsive

--- src/components/template/logo.tsx
+++ src/components/template/logo.tsx
@@ -1,16 +1,15 @@
-import { cn } from "@/lib/utils";
-
 export function Logo({ className }: Props) {
   return (
-    <div
-      className={cn(
-        "font-federo flex items-center gap-2 rounded-full text-3xl font-semibold",
-        className,
-      )}
-    >
-      <p className="bg-linear-to-br from-teal-700 to-sky-400 bg-clip-text text-transparent">
-        Maestro de la Brocha
-      </p>
+    <div className={className}>
+      <h1 className="text-primary flex flex-col items-center font-bold">
+        <span className="font-rap-script text-gradient bg-orange-gradient px-[0.2em] text-[1.8em]">
+          Maestro
+        </span>
+        <span className="font-paint-cans px-[0.3em] text-[0.6em] text-neutral-300">de la</span>
+        <span className="font-rap-script text-gradient bg-blue-gradient px-[0.25em] text-[1.8em]">
+          brocha
+        </span>
+      </h1>
     </div>
   );
 }
